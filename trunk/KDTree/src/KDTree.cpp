a9b779a9560891fcc20eaad43398f2b69c4e2e2a
huallpa.nestor <huallpa.nestor@e61a746a-3c76-0f37-829a-f636c7cc3421>
2012-05-22 04:17:16 +0000

    Agrego getoption al main

--- trunk/KDTree/src/KDTree.cpp
+++ trunk/KDTree/src/KDTree.cpp
@@ -23,14 +23,28 @@
 #include "Operacion.h"
 #include "ManejadorDeOperacion.h"
 
+using std::cout;
+using std::endl;
+
 void iniciartestFranajaHoraria(){
 	//para testear la clase FranjaHoraria
 	TestFranjaHoraria* testFranajaHoraria = new TestFranjaHoraria();
 	testFranajaHoraria->iniciar();
 }
 
-int main(int argc, char** argv){
 
+const char* nombre_programa = "KDTree";
+
+void imprime_uso() {
+	cout<<"Uso: "<<nombre_programa<<" opciones [ argumentos ...] \n"<<endl;
+    cout<<"    Opciones                    Comentarios"<<endl;
+    cout<<"    ---------------             -------------------"<<endl;
+    cout<<"    -h  --help                  Enseña esta ayuda\n"<<endl;
+    cout<<"    -i  --interfaxe             Modo interfase con usuario\n"<<endl;
+    cout<<"    -c  --carga                 Recibe como argumento un archivo para carga masiva\n"<<endl;
+}
+
+void iniciarInterfase() {
 	KDTreeController kdTreeController;
 	Menu* menu_principal = new MenuPrincipal(kdTreeController);
 	menu_principal->iniciar();
@@ -47,6 +61,88 @@ int main(int argc, char** argv){
 	delete manejador;
 
 //		iniciartestFranajaHoraria();
+}
+
+void iniciarCargaMasiva(const char* fichero_entrada) {
+    cout<<"Carga Masiva de : "<<fichero_entrada<<endl;
+}
+
+int main(int argc, char** argv){
+
+
+	int siguiente_opcion;
+
+	/* Una cadena que lista las opciones cortas válidas */
+	const char* const op_cortas = "hic:" ;
+
+	/* Una estructura de varios arrays describiendo los valores largos */
+	const struct option op_largas[] =
+	{
+	  { "help",         0,  NULL,  'h'},
+	  { "interfaxe",    0,  NULL,  'i'},
+	  { "carga",        1,  NULL,  'c'},
+	  { NULL,           0,  NULL,   0 }
+	};
+
+	/* El nombre del fichero que recibe la salida del programa */
+	const char* fichero_entrada = NULL ;
+
+	/* Bandera a activar si hay que imprimir más información
+	* en el modo verbose */
+	bool activaInterfase = 0;
+
+	/* Guardar el nombre del programa para incluirlo a la salida */
+	nombre_programa = argv[0];
+
+	/* Si se ejecuta sin parámetros ni opciones */
+	if (argc == 1)
+	{
+	  imprime_uso();
+	  exit(0);
+	}
+
+
+
+	 while (1)
+	  {
+	      /* Llamamos a la función getopt */
+	      siguiente_opcion = getopt_long (argc, argv, op_cortas, op_largas, NULL);
+
+	      if (siguiente_opcion == -1)
+	          break; /* No hay más opciones. Rompemos el bucle */
+
+	      switch (siguiente_opcion)
+	      {
+	          case 'h' : /* -h o --help */
+	              imprime_uso();
+	              exit(EXIT_SUCCESS);
+
+	          case 'i' : /* -v o --verbose */
+	        	  activaInterfase = true;
+	              break;
+
+	          case 'c' : /* -o ó --output */
+
+	              fichero_entrada = optarg;
+	              break;
+
+	          case '?' : /* opción no valida */
+	              imprime_uso(); /* código de salida 1 */
+	              exit(1);
+
+	          case -1 : /* No hay más opciones */
+	              break;
+
+	          default : /* Algo más? No esperado. Abortamos */
+	              abort();
+	          }
+	  }
+	if (activaInterfase) {
+		iniciarInterfase();
+	}
+	if (fichero_entrada) {
+		iniciarCargaMasiva(fichero_entrada);
+	}
 
 	return 0;
 }
