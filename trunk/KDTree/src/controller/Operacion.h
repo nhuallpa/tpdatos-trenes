b0ff9530ebd7161655a7688aa0888e14a9663aa5
arielcorreofiuba <arielcorreofiuba@e61a746a-3c76-0f37-829a-f636c7cc3421>
2012-05-26 03:37:39 +0000

    organizacion Menu y controlador de KDTree

---
+++ trunk/KDTree/src/controller/Operacion.h
@@ -0,0 +1,20 @@
+/*
+ * Operacion.h
+ *
+ *  Created on: 19/05/2012
+ *      Author: ariel
+ */
+
+#ifndef OPERACION_H_
+#define OPERACION_H_
+#include <iostream>
+using namespace std;
+
+class Operacion {
+public:
+	Operacion();
+	virtual ~Operacion();
+	void iniciar();
+};
+
+#endif /* OPERACION_H_ */
