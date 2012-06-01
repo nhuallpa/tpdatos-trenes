b44d56ac2b4e69c4553891a02455994c7953d821
arielcorreofiuba <arielcorreofiuba@e61a746a-3c76-0f37-829a-f636c7cc3421>
2012-06-01 04:58:33 +0000

    Terminando de actualizar test_insersion.sh

--- trunk/KDTree/Tests/test_consultas.sh
+++ trunk/KDTree/Tests/test_consultas.sh
@@ -2,74 +2,77 @@ rm datos.tree -f
 rm datos.tree.fs -f
 echo
 #Test/test_consultas.sh
-
-#Estructura de Reporte es:
-#idLinea;
-#idFormacion;
-#idFalla;
-#idAccidente;
-#FranjaHoraria* franjaHoraria;
-
-#Objetivos:
-#obtener las siguientes filtraciones
-#01) Todos las formaciones que tiene cierta falla.
-#02) Todos las formaciones que tiene cierto accidente.
-#03) Todas las fallas de una formación determinada.
-#04) Todos los accidentes de una formación determinada.
-#05) Todas las fallas.
-#06) Todas las formaciones.
-#07) Todas las lineas.
-#08) Todos los accidentes.
-#09) Todos las formaciones que tiene cierta falla  que ocurrieron dentro de una franja horaria.
-#10) Todos las formaciones que tiene cierto accidente que ocurrieron dentro de una franja horaria.
-#11) Todas las fallas de una formación determinada que ocurrieron dentro de una franja horaria.
-#12) Todos los accidentes de una formación determinada que ocurrieron dentro de una franja horaria.
-
 ################################################################################
-#formato de la consulta:
-#Programa -q "--[parametro de consulta] --[parametro de consulta]=[valor de parametro]"
-#
+#Estructura de Reporte es: idLinea|idFormacion|idFalla|idAccidente|franjaHoraria
+################################################################################
 #los pararametros de consulta posibles son:
-#lines, formacion, falla, accidente, fechaDesde, fechaHasta
-#
+#lineas, formacion, falla, accidente, fechaDesde, fechaHasta
 ################################################################################
-#valores posibles para los parametros en cuestion:
+#valores utilizados en los comandos(recursos):
+idLinea=1
+idFormacion=1
 idFalla=1
 idAccidente=1
-idFormacion=1
-#ESCAPEO CON LA CONTRABA BARRA
+franjaHoraria="2012030112001300" #(12:00-13:00-01/03/2012)
 fechaDesde="2012030113001300" #(13:00-13:00-01/03/2012)
 fechaHasta="2012090121002100" #(21:00-21:00-01/09/2012)
 
-#ejemplos de corrida de los comandoS
-#01) ../kdtree -q "--formacion --falla=$idFalla"
-#02) ../kdtree -q "--formacion --accidente=$idFormacion"
-#03) ../kdtree -q "--falla --formacion=$idFormacion"
-#04) ../kdtree -q "--accidente --formacion=$idFormacion"
-#05) ../kdtree -q "--falla"
-#06) ../kdtree -q "--formacion"
-#07) ../kdtree -q "--linea"
-#08) ../kdtree -q "--accidente"
-#09)  ../kdtree -q "--formacion --falla=$idFalla --fechaDesde=$fechaDesde --fechaHasta=$fechaHasta"
-#10)  ../kdtree -q "--formacion --accidente=$idFormacion --fechaDesde=$fechaDesde --fechaHasta=$fechaHasta"
-#11)  ../kdtree -q "--falla --formacion=$idFormacion --fechaDesde=$fechaDesde --fechaHasta=$fechaHasta"
-#12)  ../kdtree -q "--accidente --formacion=$idFormacion --fechaDesde=$fechaDesde --fechaHasta=$fechaHasta"
+function continuarCon
+{
+	echo Presione enter para continuar con el Test
+	read
+	echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+################################################################################
+echo Inicio de ejecucion de Consultas en KDTree:
+echo
+echo Test#01: Todas las formaciones que tienen falla $idFalla.
+../kdtree -q "--formacion --falla=$idFalla"
+continuarCon
 
-echo pruebas de las consultas contra el KDTree
-echo -------------------------------------------------------------------------------------------------
-echo -------------------------------------------------------------------------------------------------
-rm datos.tree -f
-rm datos.tree.fs -f 
-echo Test#09: Todos las formaciones que tiene cierta falla  que ocurrieron dentro de una franja horaria.
+echo Test#02: Todas las formaciones que tienen accidente $idAccidente.
+../kdtree -q "--formacion --accidente=$idAccidente"
+continuarCon
+
+echo Test#03: Todas las fallas que tienen formacion $idFormacion.
+../kdtree -q "--falla --formacion=$idFormacion" 
+continuarCon
+
+echo Test#04: Todos los accidentes que tienen formacion $idFormacion.
+../kdtree -q "--accidente --formacion=$idFormacion"
+continuarCon
+
+echo Test#05: Todas las fallas
+../kdtree -q "--falla"
+continuarCon
+
+echo Test#06: Todas las formaciones.
+../kdtree -q "--formacion"
+continuarCon
+
+echo Test#07: Todas las lineas.
+../kdtree -q "--linea"
+continuarCon
+
+echo Test#08: Todas los accidentes.
 ../kdtree -q "--formacion --falla=$idFalla --fechaDesde=$fechaDesde --fechaHasta=$fechaHasta"
+continuarCon
 
-#...
-echo 
-echo Presione enter para continuar...
-read
-echo Test#10: Todos las formaciones que tiene cierto accidente que ocurrieron dentro de una franja horaria.
-echo TODO: seguir poniendo los casos de pruebas...
-rm datos.tree -f
-rm datos.tree.fs -f
-#
-#fin del script
+echo Test#09: Todas las formaciones que tienen falla $idFalla que ocurrieron desde $fechaDesde hasta $fechaHasta.
+../kdtree -q "--formacion --falla=$idFalla --fechaDesde=$fechaDesde --fechaHasta=$fechaHasta"
+continuarCon
+
+echo Test#10: Todas las formaciones que tienen accidente $idAccidente que ocurrieron desde $fechaDesde hasta $fechaHasta.
+../kdtree -q "--formacion --accidente=$idAccidente --fechaDesde=$fechaDesde --fechaHasta=$fechaHasta"
+continuarCon
+
+echo Test#11: Todas las fallas que tienen formacion $idFormacion que ocurrieron desde $fechaDesde hasta $fechaHasta.
+../kdtree -q "--falla --formacion=$idFormacion --fechaDesde=$fechaDesde --fechaHasta=$fechaHasta"
+continuarCon
+
+echo Test#12: Todos los accidentes que tienen formacion $idFormacion que ocurrieron desde $fechaDesde hasta $fechaHasta.
+../kdtree -q "--accidente --formacion=$idFormacion --fechaDesde=$fechaDesde --fechaHasta=$fechaHasta"
+
+
+
+
